2b0d73e36b1788b1e6ca605d4ed5756681860a03
artsem.maroz <artsem.maroz@sharekey.com>
2022-04-11 23:37:23 +0200

    added static

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile('subRepo/subRepo/index.html', { root: __dirname });
+    res.sendFile('static/index.html', { root: __dirname });
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
